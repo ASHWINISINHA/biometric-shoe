f94d7aff6d35229db9310a7b0eb521bed19fc12f
Ashwini kumar sinha <31278395+ASHWINISINHA@users.noreply.github.com>
2017-10-04 10:23:16 +0530

    Update pizo.ino

--- pizo.ino
+++ pizo.ino
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) [year] [Ashwini kumar sinha]
+Copyright (c) [2017] [Ashwini kumar sinha]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
